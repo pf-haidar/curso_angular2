9225feb2d8bb3a825c4206fe492ac160ead16d2f
pf-haidar <pedrohaidarfilho@gmail.com>
2024-02-01 15:09:12 -0400

    feat control flow

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TemplateBindingComponent, TemplateVariablesComponent],
+  imports: [RouterOutlet, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Observable, delay, of } from 'rxjs';
+
+@Component({
+  selector: 'app-template-control-flow',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './template-control-flow.component.html',
+  styleUrl: './template-control-flow.component.scss'
+})
+export class TemplateControlFlowComponent {
+  public isTrue = false;
+  public switchCondition = 'A';
+
+  public itens: Array<{ name: string }> = [];
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3',
+  ]).pipe(delay(3000));
+
+  public addNewName(value: string) {
+    return this.itens.push({ name: value });
+  }
+}
