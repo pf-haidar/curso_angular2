52b3a22d9c70371b27f30c6e704ebe41fef4050f
pf-haidar <pedrohaidarfilho@gmail.com>
2024-02-01 16:14:14 -0400

    feat deferrable views

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -3,11 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
+import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent,],
+  imports: [RouterOutlet,
+    TemplateBindingComponent,
+    TemplateVariablesComponent,
+    TemplateControlFlowComponent,
+    TemplateDeferrableViewsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
